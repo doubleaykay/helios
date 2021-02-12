9d5642338a346d932449b2c7e1a1e3d83273db21
doubleaykay <the.a.cars@gmail.com>
2021-02-11 22:05:45 -0500

    bring dev code in

--- python/shams.py
+++ python/shams.py
@@ -1,30 +1,77 @@
-import numpy
-import suncalc # sun library
-from datetime import datetime as dt
-import pytz
+import numpy as np
+import suncalc
 import drawSvg as draw
-import helper # custom helper functions
+from datetime import datetime
 
-year = '2021'
+import colorsys
 
-# location
-# lat =
-# lon =
+def base_date_arr(year):
+    days = np.arange(str(year), str(year+1), dtype='datetime64[D]')
 
-# get time zone of location
-# array of datetimes: (year, [month 1 -> 12], [day 1 -> 31], [hour 00 -> 24], [minute 00 -> 60])
-# attach timezone to each datetime object
+    # get minutes in each day as row vector
+    lst = []
+    for day in days:
+        lst.append(np.arange(day, day+1, dtype='datetime64[m]'))
 
-# IF PRETTY PLOT
-# for each datetime, get sun position and color
+    # construct np.array
+    lst_arr = np.array(lst)
+    # transpose to get minutes as column vectors
+    arr_dt64_noflip = lst_arr.T
+    # flip array along axis 0 since SVG is filled from bottom left corner
+    arr_dt64 = np.flip(arr_dt64_noflip, axis=0)
+    
+    return arr_dt64
 
-# IF FUNCTIONAL PLOT
-# for each datetime, get sun position and color separating specific sun times
+# convert from array of datetime64 to normal datetime with the UTC timestamp attached
+def dt64_to_dtUTC_noVec(dt64):
+    dt64 = np.datetime64(dt64)
+    ts = (dt64 - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
+    return datetime.utcfromtimestamp(ts)
+dt64_to_dtUTC = np.vectorize(dt64_to_dtUTC_noVec)
 
-# SVG
-# library imported as 'draw'
-# PARAM canvas size
-# determine size of minute rectangles
-# for each datetime and color, associate with rectangle and plot color
-# add axes
-# generate and save image PARAM filename
+def pos_noVec(ts, lon, lat):
+    azi_alt = suncalc.get_position(ts, lon, lat)
+    azi = azi_alt['azimuth']
+    alt = azi_alt['altitude']
+    color = dan_color(azi, alt) # DAN!!
+    return color
+pos = np.vectorize(pos_noVec)
+
+def gen_svg(colors, d, width, height, x_tick, y_tick):
+    # x is the axis 1 index
+    # y is the axis 0 index
+    # index tuple is (y,x)
+    
+    it = np.nditer(colors, flags=['multi_index'])
+    for color in it:
+        y = it.multi_index[0] * y_tick
+        x = it.multi_index[1] * x_tick
+        r = draw.Rectangle(x, y, x_tick, y_tick, fill=f'#{color}')
+        d.append(r)
+
+
+def let_there_be_light(year, lon, lat, width, height, outfile):
+    arr_dt64 = base_date_arr(year)
+    arr_dtUTC = dt64_to_dtUTC(arr_dt64)
+    colored = pos(arr_dtUTC, lon, lat)
+
+    x_tick = width / 365 # days in a year
+    y_tick = height / 1440 # min in a day
+
+    d = draw.Drawing(width, height, displayInline=False)
+
+    gen_svg(colored, d, width, height, x_tick, y_tick)
+    d.setPixelScale(2)
+    d.savePng(outfile)
+
+    print('Done.')
+
+if __name__ == "__main__":
+    year = '2021'
+    lon = 71.0589
+    lat = 42.3601
+    width = 1920
+    height = 1080
+    outfile = 'color_test2.png'
+
+    let_there_be_light(year, lon, lat, width, height, outfile)
