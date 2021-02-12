75619980a26b0a27e70d7217bfc8f393cb753949
doubleaykay <the.a.cars@gmail.com>
2021-02-11 22:34:50 -0500

    debug and comments

--- python/shams.py
+++ python/shams.py
@@ -1,10 +1,20 @@
+"""
+Generate pretty plot of sun position for given year, lat, lon.
+
+Written by Anoush Khan & Dan Strauss
+February 2021
+"""
 import numpy as np
 import suncalc
 import drawSvg as draw
 from datetime import datetime
 
+# import to test first color algorithm
+# from test_color import *
+
 # array of timestamps
 def base_date_arr(year):
+    # get all days in year
     days = np.arange(str(year), str(year+1), dtype='datetime64[D]')
 
     # get minutes in each day as row vector
@@ -22,53 +32,67 @@ def base_date_arr(year):
     return arr_dt64
 
 # convert from array of datetime64 to normal datetime with the UTC timestamp attached
+# vectorized
 def dt64_to_dtUTC_noVec(dt64):
-    dt64 = np.datetime64(dt64)
+    dt64 = np.datetime64(dt64) # correct dtype
     ts = (dt64 - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 's')
     return datetime.utcfromtimestamp(ts)
 dt64_to_dtUTC = np.vectorize(dt64_to_dtUTC_noVec)
 
 # timestamp to sun position to color
+# vectorized
 def pos_noVec(ts, lon, lat):
+    # get sun positions
     azi_alt = suncalc.get_position(ts, lon, lat)
     azi = azi_alt['azimuth']
     alt = azi_alt['altitude']
+
+    # turn positions into colors
     color = dan_color(azi, alt) # DAN!!
     return color
+    # return pos_to_color(azi, alt) # test first color algorithm
 pos = np.vectorize(pos_noVec)
 
 # color hex codes to svg
 def gen_svg(colors, d, width, height, x_tick, y_tick):
-    # x is the axis 1 index
-    # y is the axis 0 index
     # index tuple is (y,x)
+    # # x is the axis 1 index
+    # y is the axis 0 index
     
-    it = np.nditer(colors, flags=['multi_index'])
+    it = np.nditer(colors, flags=['multi_index']) # keep track of index
     for color in it:
-        y = it.multi_index[0] * y_tick
-        x = it.multi_index[1] * x_tick
-        r = draw.Rectangle(x, y, x_tick, y_tick, fill=f'#{color}')
-        d.append(r)
+        y = it.multi_index[0] * y_tick # get y position
+        x = it.multi_index[1] * x_tick # get x position
+        r = draw.Rectangle(x, y, x_tick, y_tick, fill=f'#{color}') # create rectangle
+        d.append(r) # draw rectangle on canvas
 
 # main function
 def let_there_be_light(year, lon, lat, width, height, outfile):
-    arr_dt64 = base_date_arr(year)
-    arr_dtUTC = dt64_to_dtUTC(arr_dt64)
-    colored = pos(arr_dtUTC, lon, lat)
+    # times to sun positions to colors
+    arr_dt64 = base_date_arr(year) # numpy timestamps
+    arr_dtUTC = dt64_to_dtUTC(arr_dt64) # UTC timestamps
+    colored = pos(arr_dtUTC, lon, lat) # hex code colors
 
+    # svg parameters
     x_tick = width / 365 # days in a year
     y_tick = height / 1440 # min in a day
 
+    # svg canvas
     d = draw.Drawing(width, height, displayInline=False)
 
+    # generate svg
     gen_svg(colored, d, width, height, x_tick, y_tick)
+
+    # save svg
     d.setPixelScale(2)
     d.savePng(outfile)
 
     print('Done.')
 
 if __name__ == "__main__":
-    year = '2021'
+    # params
+    # TTD get these from command line or interface
+    year = 2021
     lon = 71.0589
     lat = 42.3601
     width = 1920
