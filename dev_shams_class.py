3b56dcf8ee9a2524e26ab167fa71809467be337a
doubleaykay <the.a.cars@gmail.com>
2021-02-14 18:41:47 -0500

    init file for class development

---
+++ dev_shams_class.py
@@ -0,0 +1,18 @@
+class Shams:
+    def __init__():
+        # init params
+        # use kwargs for default values such as hue_shift or use_DST
+        pass
+
+    def update_params():
+        # public
+        # kwargs with values = self values
+        # that way, if the function is called with no args,
+        # nothing will happen
+        # and, any combination of params can be updated at a given time
+        pass
+
+    def gen_png():
+        # public
+        # generate png
+        pass
